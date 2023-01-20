c9f0b4bbc67b0e9d9daf871b58b1b7016b8c77e1
Iman Far <iman.khaghani@gmail.com>
2023-01-19 20:15:22 -0500

    midwork

--- ui/src/container/layout-container.tsx
+++ ui/src/container/layout-container.tsx
@@ -1,8 +1,15 @@
 import {LoginComponent} from "../component/login-component";
+import {Col, Container, Row} from "react-bootstrap";
 
 export const LayoutContainer = () =>
     <>
-        <LoginComponent />
+        <Container fluid>
+            <Row>
+                <Col>
+                    <LoginComponent />
+                </Col>
+            </Row>
+        </Container>
     </>
 
 
